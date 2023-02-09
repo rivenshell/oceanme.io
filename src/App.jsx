3aa4f1256633c3ccccdfce4c89887e17b45d0020
Rivenshell <rivenshell111@gmail.com>
2023-02-09 01:36:07 -0500

    Site Template Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,22 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 import Casestudy from "./components/casestudy";
+import Navbar from "./components/navbar";
+import Dashboard from "./components/dashboard";
+
+
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1> Oceam-me</h1>
-      // main content
-      <div className="container">
-        <div className="row">
-          <div className="col-12">
-            <Casestudy />
-          </div>
-        </div>
-      </div>
-      {/* footer */}
-      <footer className="footer mt-auto py-3 bg-light">
-        <div className="container">
-          <span className="text-muted">React App</span>
-        </div>
-      </footer>
+    <Dashboard />
+     
+      
     </>
   );
 }
 
 export default App;
+
---
+++ src/components/dashboard.jsx
@@ -0,0 +1,95 @@
+import React from 'react'
+import { FileOutlined, PieChartOutlined, UserOutlined } from '@ant-design/icons';
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
+import { useState } from 'react';
+const { Header, Content, Footer, Sider } = Layout;
+function getItem(label, key, icon, children) {
+  return {
+    key,
+    icon,
+    children,
+    label,
+  };
+}
+
+
+const items = [
+    getItem('Option 1', '1', <PieChartOutlined />),
+    // getItem('Option 2', '2', <DesktopOutlined />),
+    getItem('User', 'sub1', <UserOutlined />, [
+      getItem('Tom', '3'),
+      getItem('Bill', '4'),
+      getItem('Alex', '5'),
+    ]),
+    getItem('Team', 'sub2', <UserOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
+    getItem('Files', '9', <FileOutlined />),
+  ];
+
+
+function dashboard() {
+    const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+  return (
+   <>
+    <Layout
+      style={{
+        minHeight: '100vh',
+      }}
+    >
+      <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+        <div
+          style={{
+            height: 32,
+            margin: 16,
+            background: 'rgba(255, 255, 255, 0.2)',
+          }}
+        />
+        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
+      </Sider>
+      <Layout className="site-layout">
+        <Header
+          style={{
+            padding: 0,
+            background: colorBgContainer,
+          }}
+        />
+        <Content
+          style={{
+            margin: '0 16px',
+          }}
+        >
+          <Breadcrumb
+            style={{
+              margin: '16px 0',
+            }}
+          >
+            <Breadcrumb.Item>User</Breadcrumb.Item>
+            <Breadcrumb.Item>Riv</Breadcrumb.Item>
+          </Breadcrumb>
+          <div
+            style={{
+              padding: 24,
+              minHeight: 360,
+              background: colorBgContainer,
+            }}
+          >
+            Bill is a cat.
+          </div>
+        </Content>
+        <Footer
+          style={{
+            textAlign: 'center',
+          }}
+        >
+           ©2023 ATOM -- Creative Development Agency
+        </Footer>
+      </Layout>
+    </Layout>
+   </> 
+  )
+}
+
+export default dashboard
+
